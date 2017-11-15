c9f5855f03c2ca62fe5ac8058a3b1016dcf89438
Ranjana1996 <ranjana.seshadri@gmail.com>
2017-11-15 18:51:18 +0530

    First commit

---
+++ app/src/main/java/com/example/nithinrama/pill_bottle/Start_activity.java
@@ -0,0 +1,14 @@
+package com.example.nithinrama.pill_bottle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Start_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_activity);
+
+    }
+}
